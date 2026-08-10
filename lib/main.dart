b0b6bb24abe3b89b56767cb1a2deea356e927d2d
Bereket-Ketema <bereketketema866@gmail.com>
2026-08-11 00:42:02 +0300

    the main app have been updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:product_7/features/ecommerce/presentation/screens/add_edit.dart';
+import 'package:product_7/features/ecommerce/presentation/screens/detail.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'features/ecommerce/data/datasources/product_local_data_source_impl.dart';
+//import 'features/ecommerce/data/datasources/product_remote_data_source.dart';
+import 'features/ecommerce/data/datasources/product_remote_data_source_impl.dart';
+import 'features/ecommerce/data/repositories/product_repository_impl.dart';
+import 'features/ecommerce/domain/repositories/product_repository.dart';
 import 'features/ecommerce/presentation/screens/home.dart';
-import 'features/ecommerce/presentation/screens/add_edit.dart';
-import 'features/ecommerce/presentation/screens/detail.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final sharedPreferences = await SharedPreferences.getInstance();
+  runApp(MyApp(repository: _initRepository(sharedPreferences)));
+}
+
+ProductRepository _initRepository(SharedPreferences sharedPreferences) {
+  final remoteDataSource = ProductRemoteDataSourceImpl();
+  final localDataSource = ProductLocalDataSourceImpl(
+    sharedPreferences: sharedPreferences,
+  );
+  return ProductRepositoryImpl(
+    remoteDataSource: remoteDataSource,
+    localDataSource: localDataSource,
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final ProductRepository repository;
+
+  const MyApp({super.key, required this.repository});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Product App',
+      title: 'Shopera',
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomeScreen(),
-        '/add-edit': (context) => const AddEditScreen(),
-        '/detail': (context) => const DetailScreen(),
+        '/': (context) => HomeScreen(repository: repository),
+        '/add-edit': (context) =>  AddEditScreen(repository: repository,),
+        '/detail': (context) => DetailScreen(repository: repository,),
       },
     );
   }
